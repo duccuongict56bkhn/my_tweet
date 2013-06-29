5f06f03a3e2a6ca0718f572950a484b3b628072b
Duc Cuong <duccuong5199@gmail.com>
2013-06-29 09:23:06 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTweet::Application.config.secret_token = '05d2db8d7d6fafe9a181055f156266ae8bd68a1b66d86e7b19f88f314adfe045e109efac0e81c317c527097b2c300004fc397a35ea7db781a5fca0c9d8d11e14'
