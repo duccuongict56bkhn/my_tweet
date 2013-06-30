bebe58bf967160425a313b4605eda3b918968b0a
Duc Cuong <duccuong5199@gmail.com>
2013-06-30 09:58:38 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MyTweet::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: "static_pages#home"
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name "Dao Duc Cuong"
+		email "duccuong5199@gmail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
